8415795f43e37a9046dde698c074ea88b526a2a4
AleDore <alessio85.dore@gmail.com>
2024-10-03 17:00:06 +0200

    add termination flag

--- scripts/ops/src/commands/check_blob_migration.ts
+++ scripts/ops/src/commands/check_blob_migration.ts
@@ -2,8 +2,10 @@
 import { BlobServiceClient } from "@azure/storage-blob";
 import { pipe } from "fp-ts/lib/function";
 import * as TE from "fp-ts/TaskEither";
+import * as O from "fp-ts/Option";
+import * as B from "fp-ts/boolean";
 
-import { Args, Command } from "@oclif/core";
+import { Args, Command, Flags } from "@oclif/core";
 import { createContainerClientIfNotExistsOrGet } from "../utils/storage";
 import { getCheckpoint, getSaveBlobCheckpoint } from "../utils/checkpoint";
 
@@ -34,8 +36,21 @@ export default class CheckBlobMigration extends Command {
     }),
   };
 
+  public static flags = {
+    tagName: Flags.string({
+      char: "t",
+      description: "Specify the tag name to check",
+      required: true,
+    }),
+    tagValue: Flags.string({
+      char: "v",
+      description: "Tag value to compare",
+      required: true,
+    }),
+  };
+
   public async run(): Promise<void> {
-    const { args } = await this.parse(CheckBlobMigration);
+    const { args, flags } = await this.parse(CheckBlobMigration);
 
     const targetClient = BlobServiceClient.fromConnectionString(
       args.targetStorage,
@@ -63,38 +78,58 @@ export default class CheckBlobMigration extends Command {
     this.log(`checkpoint is ${JSON.stringify(checkpoint)}`);
 
     let skip = true;
+    let flagTerm = false;
     const alreadyVisitedContainers = checkpoint?.alreadyVisitedContainers ?? [];
     for await (const container of targetClient.listContainers()) {
-      skip =
-        alreadyVisitedContainers.includes(container.name) &&
-        container.name !== checkpoint?.lastContainerName &&
-        skip;
-      if (!skip) {
-        await saveBlobCheckPoint(alreadyVisitedContainers, container.name)();
-        const containerClient = targetClient.getContainerClient(container.name);
+      if (!flagTerm) {
+        skip =
+          alreadyVisitedContainers.includes(container.name) &&
+          container.name !== checkpoint?.lastContainerName &&
+          skip;
+        if (!skip) {
+          await saveBlobCheckPoint(alreadyVisitedContainers, container.name)();
+          const containerClient = targetClient.getContainerClient(
+            container.name,
+          );
 
-        let i = 1;
-        for await (const response of containerClient
-          .listBlobsFlat()
-          .byPage({
+          let i = 1;
+          for await (const response of containerClient.listBlobsFlat().byPage({
             continuationToken: checkpoint?.continuationToken,
             maxPageSize: 1,
           })) {
-          for (const blob of response.segment.blobItems) {
-            this.log(`Blob ${i++}: ${blob.name}`);
+            for (const blob of response.segment.blobItems) {
+              flagTerm = pipe(
+                blob.tags,
+                O.fromNullable,
+                O.map((tags) => tags[flags.tagName] !== flags.tagValue),
+                O.getOrElse(() => false),
+              );
+              if (flagTerm) {
+                this.log(`Migration not completed yet!`);
+                break;
+              }
+            }
+            if (flagTerm) {
+              break;
+            }
+            await saveBlobCheckPoint(
+              alreadyVisitedContainers,
+              container.name,
+              response.continuationToken,
+            )();
           }
-          await saveBlobCheckPoint(
-            alreadyVisitedContainers,
-            container.name,
-            response.continuationToken,
-          )();
+          if (flagTerm) {
+            break;
+          }
+          if (!alreadyVisitedContainers.includes(container.name)) {
+            alreadyVisitedContainers.push(container.name);
+          }
+          await saveBlobCheckPoint(alreadyVisitedContainers, container.name)();
         }
-        if (!alreadyVisitedContainers.includes(container.name)) {
-          alreadyVisitedContainers.push(container.name);
-        }
-        await saveBlobCheckPoint(alreadyVisitedContainers, container.name)();
       }
     }
-    await saveBlobCheckPoint(alreadyVisitedContainers, "undefined")();
+    if (!flagTerm) {
+      await saveBlobCheckPoint(alreadyVisitedContainers, "undefined")();
+    }
   }
 }
